9f1306f427e590f69575bb9a105bfddb633e874a
James Nguyen <james.nguyenn96@gmail.com>
2022-06-10 15:33:49 +1000

    Create basic javascript functionality for button

---
+++ index.js
@@ -0,0 +1,16 @@
+// var associate = document.getElementsByClassName('associate');
+
+// associate.onclick(turnRed());
+
+// function turnRed() {
+//     console.log("hello");
+// }
+
+var popup = document.getElementById("popup");
+
+document.getElementById('hel').onclick = function sayHello() {
+
+    popup.style.display = "block";
+ 
+ }
+
