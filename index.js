1a3515fd731bffb57136edf002cda9b923aee34d
James Nguyen <james.nguyenn96@gmail.com>
2022-06-24 14:55:56 +1000

    Add new function to close popup

--- index.js
+++ index.js
@@ -7,10 +7,17 @@
 // }
 
 var popup = document.getElementById("popup");
+var close = document.getElementById("close");
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+
+}
 
 function openPopup() {
     popup.classList.add("open-popup");
 }
+
 
 // document.getElementById('hel').onclick = function sayHello() {
 
