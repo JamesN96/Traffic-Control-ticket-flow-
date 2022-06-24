23476615f8194a0f960ff65e7a02443ece2a824c
James Nguyen <james.nguyenn96@gmail.com>
2022-06-24 15:52:53 +1000

    Add in functionality for pop up box for major html webpage

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var popup = document.getElementById("popup");
 var close = document.getElementById("close");
 var scg = document.getElementById("popup-scg");
 var osmo = document.getElementById("osmo");
+var L2 = document.getElementById("L2");
+
 
 
 function closePopup() {
@@ -28,3 +30,12 @@ function openOsmo() {
 function closeOsmo() {
     osmo.classList.remove("open-popup");
 }
+
+function openL2() {
+    L2.classList.add("open-popup");
+}
+
+function closeL2() {
+    L2.classList.remove("open-popup");
+}
+
