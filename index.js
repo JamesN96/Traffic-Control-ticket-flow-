dfe50caee8651421a4eb96986872461d63e2e004
James Nguyen <james.nguyenn96@gmail.com>
2022-07-01 15:56:33 +1000

    Create new element selectors and create new functions to implement popup functionality

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var permissions = document.getElementById("permissions");
 var laps = document.getElementById("laps");
 var basic = document.getElementById("basic");
 var cw = document.getElementById("cw");
-var cloud = document.getElementById("L2");
-var networking = document.getElementById("L2");
-var password = document.getElementById("L2");
+var cloud = document.getElementById("cloud");
+var networking = document.getElementById("networking");
+var password = document.getElementById("password");
 var p3 = document.getElementById("p3");
 
 
@@ -49,3 +49,83 @@ function closeL2() {
     L2.classList.remove("open-popup");
 }
 
+// OSMO
+
+function openOffice() {
+    office.classList.add("open-popup");
+}
+function closeOffice() {
+    office.classList.remove("open-popup");
+}
+
+function openLastPass() {
+    lastpass.classList.add("open-popup")
+}
+
+function closeLastPass() {
+    lastpass.classList.remove("open-popup");
+}
+
+function openPermissions() {
+    permissions.classList.add("open-popup")
+}
+
+function closePermissions() {
+    permissions.classList.remove("open-popup");
+}
+
+function openLaps() {
+    laps.classList.add("open-popup")
+}
+
+function closeLaps() {
+    laps.classList.remove("open-popup");
+}
+
+function openBasic() {
+    basic.classList.add("open-popup")
+}
+
+function closeBasic() {
+    basic.classList.remove("open-popup");
+}
+
+function openCw() {
+    cw.classList.add("open-popup")
+}
+
+function closeCw() {
+    cw.classList.remove("open-popup");
+}
+
+function openCloud() {
+    cloud.classList.add("open-popup")
+}
+
+function closeCloud() {
+    cloud.classList.remove("open-popup");
+}
+
+function openNetworking() {
+    networking.classList.add("open-popup")
+}
+
+function closeNetworking() {
+    networking.classList.remove("open-popup");
+}
+
+function openPassword() {
+    password.classList.add("open-popup")
+}
+
+function closePassword() {
+    password.classList.remove("open-popup");
+}
+
+function openP3() {
+    p3.classList.add("open-popup")
+}
+
+function closeP3() {
+    p3.classList.remove("open-popup");
+}
